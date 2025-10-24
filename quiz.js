a2166edab8402c3f33a947e52e3f0d8a8dd73c3b
AngiMangy <kickmango16@gmail.com>
2025-10-24 07:18:08 -0400

    pushin all to github

---
+++ quiz.js
@@ -0,0 +1,39 @@
+// Global Variables
+const question = document.getElementById("question")
+const option1 = document.getElementById("option1")
+const option2 = document.getElementById("option2")
+const option3 = document.getElementById("option3")
+const option4 = document.getElementById("option4")
+
+// Questions and Answers
+let questions = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    ""
+]
+
+
+let Answers = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+]
+
+// 
+function labelChangeTest() {
+
+}
